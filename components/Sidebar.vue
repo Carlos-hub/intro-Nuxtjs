9b0993b26db2559a03956cdc66079d9de86904d9
carlos-hub <delaracarlosmiguel@gmail.com>
2022-11-09 16:31:44 -0300

    ajustando sidebar

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,13 +1,36 @@
 <template lang="">
-    <div class="sidebar h-screen bg-[#3D9CF3] w-[103px]">
-        <h1>teste</h1>
-    </div>
+    <section class="container h-screen bg-[#3D9CF3] w-24   justify-center py-4 fixed">
+
+        <div class="w-30 h-13  ml-7 mb-12">
+            <nuxt-link to="" ><img src="svg/interactive.svg" alt="" srcset="" class=""></nuxt-link>
+        </div>
+        <div class="w-30 h-13  ml-2 p-4 my-4 hover:bg-[#fff]">
+            <nuxt-link to="" class="w-9 h-9"><img src="svg/home.svg" alt="" srcset="" class=""></nuxt-link>
+        </div>
+        <div class="w-30 h-13  ml-2 p-4 my-4 hover:bg-[#fff]">
+            <nuxt-link to="" class="w-9 h-9"><img src="svg/user.svg" alt="" srcset="" class=""></nuxt-link>
+        </div>
+        <div class="w-30 h-13  ml-2 p-4 my-4 hover:bg-[#fff]">
+            <nuxt-link to="" class="w-9 h-9"><img src="svg/comment.svg" alt="" srcset="" class=""></nuxt-link>
+        </div>
+        <div class="w-30 h-13  ml-2 p-4 my-4 hover:bg-[#fff]">
+            <nuxt-link to="" class="w-9 h-9"><img src="svg/interrogation.svg" alt="" srcset="" class=""></nuxt-link>
+        </div>
+        <div class="w-30 h-13  ml-2 p-4 my-4 hover:bg-[#fff]">
+            <nuxt-link to="" class="w-9 h-9"><img src="svg/settings.svg" alt="" srcset="" class=""></nuxt-link>
+        </div>
+        <div class="w-30 h-13  ml-2 p-4 my-4 hover:bg-[#fff]">
+            <nuxt-link to="" class="w-9 h-9"><img src="svg/lock.svg" alt="" srcset="" class=""></nuxt-link>
+        </div>
+        <div class="w-30 h-13  ml-2 p-4 my-4 hover:bg-[#fff]">
+            <nuxt-link to="" class="w-9 h-9"><img src="svg/sign-out-alt.svg" alt="" srcset="" class=""></nuxt-link>
+        </div>
+
+        
+    </section>
 </template>
 <script>
 export default {
     
 }
 </script>
-<style lang="">
-    
-</style>
