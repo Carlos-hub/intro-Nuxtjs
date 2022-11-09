2de7c24dc27860fe0c4083eb0b7d9a47cc580065
carlos-hub <delaracarlosmiguel@gmail.com>
2022-11-09 14:44:58 -0300

    Iniciando projeto dashboard em nuxtJS

---
+++ components/Sidebar.vue
@@ -0,0 +1,13 @@
+<template lang="">
+    <div class="sidebar h-screen bg-[#3D9CF3] w-[103px]">
+        <h1>teste</h1>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
