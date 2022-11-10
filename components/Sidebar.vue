a6aedb06bc62646f81593b2b902312ef2100bf1c
Carlos-hub <delaracarlosmiguel@gmail.com>
2022-11-09 21:29:43 -0300

    Ajustando botão sidebar

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,41 +1,85 @@
 <template lang="">
     <section class="">
-        <div class="float-left container h-screen bg-[#3D9CF3] w-24   justify-center py-4 fixed block">
+        <div id="sidebg" class="float-left container h-screen bg-[#3D9CF3] w-24   justify-center py-4 fixed block">
 
             <div class="w-30 h-13  ml-7 pb-12">
-                <nuxt-link to="" ><img src="svg/interactive.svg" alt="" srcset="" class=""></nuxt-link>
+                <a to="" >
+                    <img src="svg/interactive.svg" alt="" srcset="" class="">
+                    <p class="desc">XCode Sistemas</p>
+                </a>
+                
             </div>
             <div class="w-30 h-13  ml-2 p-4 my-4 hover:bg-[#fff]">
-                <nuxt-link to="" class="w-6 h-6"><img src="svg/home.svg" alt="" srcset="" class=""></nuxt-link>
+                <nuxt-link to="" class="w-6 h-6">
+                    <img src="svg/home.svg" alt="" srcset="" class="">
+                    <p class="desc">Painel</p>
+                </nuxt-link>
             </div>
             <div class="w-30 h-13  ml-2 p-4 my-4 hover:bg-[#fff]">
-                <nuxt-link to="" class="w-6 h-6"><img src="svg/user.svg" alt="" srcset="" class=""></nuxt-link>
+                <nuxt-link to="" class="w-6 h-6">
+                    <img src="svg/user.svg" alt="" srcset="" class="">
+                    <p class="desc">Clientes</p>
+                </nuxt-link>
             </div>
             <div class="w-30 h-13  ml-2 p-4 my-4 hover:bg-[#fff]">
-                <nuxt-link to="" class="w-6 h-6"><img src="svg/comment.svg" alt="" srcset="" class=""></nuxt-link>
+                <nuxt-link to="" class="w-6 h-6">
+                    <img src="svg/comment.svg" alt="" srcset="" class="">
+                    <p class="desc">Mensagens</p>
+                </nuxt-link>
             </div>
             <div class="w-30 h-13  ml-2 p-4 my-4 hover:bg-[#fff]">
-                <nuxt-link to="" class="w-6 h-6"><img src="svg/interrogation.svg" alt="" srcset="" class=""></nuxt-link>
+                <nuxt-link to="" class="w-6 h-6">
+                    <img src="svg/interrogation.svg" alt="" srcset="" class="">
+                    <p class="desc">Ajuda</p>
+                </nuxt-link>
             </div>
             <div class="w-30 h-13  ml-2 p-4 my-4 hover:bg-[#fff]">
-                <nuxt-link to="" class="w-6 h-6"><img src="svg/settings.svg" alt="" srcset="" class=""></nuxt-link>
+                <nuxt-link to="" class="w-6 h-6">
+                    <img src="svg/settings.svg" alt="" srcset="" class="">
+                    <p class="desc">Configurações</p>
+                </nuxt-link>
             </div>
             <div class="w-30 h-13  ml-2 p-4 my-4 hover:bg-[#fff]">
-                <nuxt-link to="" class="w-6 h-6"><img src="svg/lock.svg" alt="" srcset="" class=""></nuxt-link>
+                <nuxt-link to="" class="w-6 h-6">
+                    <img src="svg/lock.svg" alt="" srcset="" class="">
+                    <p class="desc">Senha</p>
+                </nuxt-link>
             </div>
             <div class="w-30 h-13  ml-2 p-4 my-4 hover:bg-[#fff]">
-                <nuxt-link to="" class="w-6 h-6"><img src="svg/sign-out-alt.svg" alt="" srcset="" class=""></nuxt-link>
+                <nuxt-link to="" class="w-6 h-6">
+                    <img src="svg/sign-out-alt.svg" alt="" srcset="" class="">
+                    <p class="desc">Sair</p>
+                </nuxt-link>
             </div>
         </div>
         <div class="clear-both"></div>        
-        <a href="" class="relative">
+        <button class="relative" v-on:click="showMenu()">
             <img src="svg/burger.svg" alt="" srcset="" class="w-10 h-10 ml-28 my-3">
-        </a>
+        </button>
 </section>
    
 </template>
+<style>
+.desc{
+    display: none;
+}
+</style>
 <script>
-export default {
+    export default {
+    methods:{
+        showMenu() {
+            
+            document.getElementById('sidebg').style.width = '400px';
+            // document.getElementsByTagName('p').style.display = 'block';
+        }
+
+    },
+    data(){
+
+        return 
+            {showMenu}
+    }
     
+
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -9,6 +9,7 @@
 import Shortcut from '../components/Shortcut.vue';
 import Sidebar from '../components/Sidebar.vue';
 
+
 export default {
     name: "IndexPage",
     components: { Sidebar, Shortcut }
